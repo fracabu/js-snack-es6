e72d5ab239585d4f323ee513cc6c2286bf42b820
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-06 16:26:19 +0200

    bici

---
+++ Snack 1/script.js
@@ -0,0 +1,47 @@
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
+
+let elencoBiciDaCorsa = [
+    { 
+        nome: "bici 1",
+        peso: "7",
+    
+    },
+
+
+    { 
+        nome: "bici 2",
+        peso: "8",
+    },
+
+    { 
+        nome: "bici 3",
+        peso: "10",
+    },
+
+    { 
+        nome: "bici 4",
+        peso: "9",
+    },
+
+    { 
+        nome: "bici 5",
+        peso: "6",
+    },
+    
+];
+
+
+
+let biciLeggera = elencoBiciDaCorsa[0];
+
+for (let i = 0; i < elencoBiciDaCorsa.length; i++) {
+    if (elencoBiciDaCorsa[i].peso < biciLeggera.peso) {
+        biciLeggera = elencoBiciDaCorsa[i];
+    }
+}
+console.log (elencoBiciDaCorsa);
+console.log(biciLeggera);
